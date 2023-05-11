9a0b66b0396c924f71fbc3c5cff993ca10bc7211
Ahmad Ado <ahmadado6002@gmail.com>
2023-05-11 18:07:23 +0100

    i used fontawesome

--- App.js
+++ App.js
@@ -9,56 +9,69 @@ import { View,
 StatusBar, 
 TextInput,
 Button} from "react-native";
+import { sampleData} from './assets/data/sample-data';
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 //leap year calculator
-export default function App () {
-  return(
-   <SafeAreaView style={styles.container}>
-    
-      <View style={styles.wrapper}>
-      <View style={styles.header}>
-        <View style={styles.leftheader}>
-          <Image source={require('./assets/charityapp.png')}
-          alt='app logo'
-          style={styles.logo}/>
-          <Text style={styles.brandName}>CharityApp</Text>
-        </View>
-
-        <Image source={require('./assets/user.png')}
-        alt='app logo'
-        style={styles.headerIcon}/>
-      </View>
-
-      <View style={styles.body}>
-        <View style={styles.actionBlock}>
-          <View style={styles.actionBox}>
-            
-          </View>
-          <View style={styles.actionBox}>
-            
-          </View>
-          <View style={styles.actionBox}>
-            
-          </View>
-          <View style={styles.actionBox}>
-            
-          </View>
-        </View>
-
-        <View style={styles.recent}>
-            <View style={styles}>
-              <View style={styles.donateDetails}>
-                <Text style={styles.donationAmount}>₦1200</Text>
-                <Text style={styles.donationInfo}> 1 minutes ago</Text>
-              </View>
-            </View>
-            <Text style={styles.donateBy}>Donate by sample@gmail.com</Text>
-        </View>
-      </View>
+  export default function App () {
+    return(
+    <SafeAreaView style={styles.container}>
       
-      </View>
-    </SafeAreaView>
-  )
-}
+        <View style={styles.wrapper}>
+        <View style={styles.header}>
+          <View style={styles.leftheader}>
+            <Image source={require('./assets/charityapp.png')}
+            alt='app logo'
+            style={styles.logo}/>
+            <Text style={styles.brandName}>CharityApp</Text>
+          </View>
+
+          <FontAwesomeIcon icon={faUser}
+           color='#5C469C' size={36}/>
+        </View>
+
+        <View style={styles.body}>
+          <View style={styles.actionBlock}>
+            <View style={styles.actionBox}>
+              
+            </View>
+            <View style={styles.actionBox}>
+              
+            </View>
+            <View style={styles.actionBox}>
+              
+            </View>
+            <View style={styles.actionBox}>
+              
+            </View>
+          </View>
+
+          <View style={styles.recent}>
+            <Text style={styles.recentTitle}>Recent Donation</Text>
+             <View style={styles.recentScroll}>
+                <FlatList
+                data={sampleData}
+                renderItem={({item}) => {
+                  return(
+                    <View style={styles.recentblock}>
+                     <View style={styles.donateDetails}>
+                      <Text style={styles.donationAmount}>₦{item.amount}</Text>
+                      <Text style={styles.donationInfo}> {item.time} minutes ago</Text>
+                      </View>
+                      <Text style={styles.donateBy}>Donate by {item.email}</Text>
+                    </View>
+                  )
+                }}
+                key={({item}) => item.id}
+                showsVerticalScrollIndicator={false}/>
+             </View>
+          </View>
+          </View>
+          
+          </View>
+        </SafeAreaView>
+    )
+  }
 
 const styles = StyleSheet.create({
   container:{
@@ -108,7 +121,10 @@ const styles = StyleSheet.create({
   },
   recent:{
     flex:3.5,
-    
+    marginTop:8,
+    padding:8,
+    borderRadius:8,
+    backgroundColor:'#FDE2F3',
   },
   actionBox:{
     width:'49%',
@@ -119,5 +135,35 @@ const styles = StyleSheet.create({
   wrapper:{
     flex:1,
     padding:12
+  },
+  recentblock:{
+    backgroundColor:'#5C469C',
+    paddingHorizontal:6,
+    paddingVertical:8,
+    gap:4,
+    borderRadius:8,
+    marginBottom:3
+  },
+  recentTitle:{
+    fontSize:22,
+    marginBottom:2
+  },
+  donateBy:{
+    color:'#D4ADFC'
+  },
+  donateDetails:{
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+  donationInfo:{
+    color:'#D4ADFC'
+  },
+  donationAmount:{
+    fontSize:20,
+    color:'#fff'
+  },
+  recentScroll:{
+    flex:1,
+    flexDirection:'column'
   }
 })
